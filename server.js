dc7bc68e1e9017235f1cfd1868dd25c88591f30b
Rick Ansay <rick.ansay@gmail.com>
2022-01-20 21:56:14 -0700

    Added front end, created tables

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-// const express = require('express');
-// const connection = require('./config');
-
-// const connections = require('./config');
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// // turn on body-parser.
-// // makes req.body exist
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// //post - create todos
-// // async await
-// // Declaring a function as "async" allows us to use "await" syntax inside of that function. 
-// app.post('./api/todos', async (req, res) => {
-//     // { task: 'Sleep'}
-//     const { task } = req.body;
-
-//     if(!task) {
-//         return res.status(404).json({ error: 'You must provide a task'});
-//     }
-//     // If there is a test save it to the db
-//     try {
-//         const insertQuery = 'INSERT INTO todos(task) VALUES (?);';
-//         const result = await connection.query(insertQuery, [task]);
-//         res.json(result);
-//     } catch (e) {
-//         res.status(400).json(e);
-//     }
-// })
-
-// app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
 const express = require('express');
 const connection = require('./config');
 // 3306
@@ -43,42 +7,47 @@ const app = express();
 // makes req.body exist
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('public'));
+// USER API's
 app.post('/api/users', async (req, res) => {
-    const { username } = req.body;
-    if (!username) {
-        return res.status(400).json({ error: 'You must provide a username'});
-    }
-    try {
-        const createUserQuery = 'INSERT INTO users(username) VALUES(?);';        
-        const getUserByIdQuery = 'SELECT * FROM users WHERE id = ?;';
-
-        const [result] = await connection.query(createUserQuery, [username]);
-        const [userResult] = await connection.query(getUserByIdQuery, [result.insertId]);
-        res.json(userResult[0]);
-
-    } catch (e) {
-        res.status(400).json(e);
-    }
-})
-
-app.get('/api/todos', async (req, res) => {
-    try {
-        const getAllTodosQuery = 'SELECT * FROM todos;';
-        const [ todos ] = await connection.query(getAllTodosQuery);
-        res.json(todos);
-    } catch (e) {
-        res.status(400).json(e);
-    }
+  const { username } = req.body;
+  if (!username) {
+    return res.status(400).json({ error: 'You must provide a username'});
+  }
+  try {
+    const createUserQuery = 'INSERT INTO users(username) VALUES(?);';
+    const getUserByIdQuery = 'SELECT * FROM users WHERE id = ?;';
+    const [result] = await connection.query(createUserQuery, [username]);
+    const [userResult] = await connection.query(getUserByIdQuery, [result.insertId]);
+    res.json(userResult[0]);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
-
-
+app.get('/api/users', async (req, res) => {
+  try {
+    const getAllUsersQuery = 'SELECT * FROM users;';
+    const [ users ] = await connection.query(getAllUsersQuery);
+    res.json(users);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+app.get('/api/todos', async (req, res) => {
+  try {
+    const getAllTodosQuery = 'SELECT todos.id, task, completed, userId, username FROM todos LEFT JOIN users ON todos.userId = users.id;';
+    const [ todos ] = await connection.query(getAllTodosQuery);
+    res.json(todos);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
 // POST - create todo
 // async await
 // Declaring a function as "async" allows us to use "await" syntax inside of that function
 app.post('/api/todos', async (req, res) => {
   // { task: 'Sleep' }
-  const { task } = req.body;
+  const { task, userId } = req.body;
   // If the user does not provide a task, respond with an error
   if(!task) {
     return res.status(400).json({ error: 'You must provide a task'});
@@ -89,16 +58,48 @@ app.post('/api/todos', async (req, res) => {
 //  put that error in the "catch" block, and then run the code in the "catch" block
   try {
   // many lines of code....
-    const insertQuery = 'INSERT INTO todos(task) VALUES(?);';
+    const insertQuery = 'INSERT INTO todos(task, userId) VALUES(?,?);';
     const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
-    const [result,] = await connection.query(insertQuery, [task]);
+    const [result,] = await connection.query(insertQuery, [task, userId,]);
     // Whenever we do an INSERT, UPDATE, OR DELETE query in mysql2 or mysql npm package
     // it doesn't give us the data that was interacted with. it instead tells us information
-    // about how many rows were affected and maybe the insertId or updateId of the regarding data.
+    // about how many rows were affected and maybe the insertId  of the regarding data.
     // It also gives us an array with 2 elements. The 1st one is an object where we have the information we need
     // 2nd one is null or information about the fields of that row
-    const todosResult = await connection.query(getTodoById, [result.insertId]);
-    res.json(todosResult);
+    const [todosResult] = await connection.query(getTodoById, [result.insertId]);
+    res.json(todosResult[0]);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+app.patch('/api/todos/:todoId', async (req, res) => {
+  const { todoId } = req.params;
+  const { task, completed } = req.body;
+  if (!task || !completed) {
+    return res.status(400).json({ error: 'You must provide the task and completed'});
+  }
+  try {
+    const updateTodoById = 'UPDATE todos SET task = ?, completed = ? WHERE id = ?';
+    const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
+    const isCompleted = completed.toLowerCase() === 'true' ? 1 : 0;
+    await connection.query(updateTodoById, [task, isCompleted, todoId ]);
+    const [todos] = await connection.query(getTodoById, todoId);
+    res.json(todos[0]);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+app.delete('/api/todos/:todoId', async (req, res) => {
+  const { todoId } = req.params;
+  try {
+    const getTodoById = 'SELECT * FROM todos WHERE id = ?;';
+    const deleteTodoById = 'DELETE FROM todos WHERE id = ?;';
+    const [todos] = await connection.query(getTodoById, todoId);
+    if (!todos[0]) {
+      return res.status(404).json({ error:'Todo not found'});
+    }
+    await connection.query(deleteTodoById, todoId);
+    res.json(todos[0]);
   } catch (e) {
     res.status(400).json(e);
   }
